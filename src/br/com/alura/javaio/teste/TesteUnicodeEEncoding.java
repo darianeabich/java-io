7cddf20de028c98d7b91ff0a25a3953c3b7f65bf
Dariane Abich <darianeabich@gmail.com>
2021-11-22 14:52:42 -0300

    Ajustes encoding no windows

--- src/br/com/alura/javaio/teste/TesteUnicodeEEncoding.java
+++ src/br/com/alura/javaio/teste/TesteUnicodeEEncoding.java
@@ -13,18 +13,18 @@ public class TesteUnicodeEEncoding {
         System.out.println(charset.displayName());
 
         byte[] bytes = s.getBytes("windows-1252");
-        System.out.println(bytes.length + ", windows-1252");
-        String sNovo = new String(bytes);
+        System.out.print(bytes.length + ", windows-1252, ");
+        String sNovo = new String(bytes, "windows-1252");
         System.out.println(sNovo);
 
         bytes = s.getBytes("UTF-16");
-        System.out.println(bytes.length + ", UTF-16");
-        sNovo = new String(bytes);
+        System.out.print(bytes.length + ", UTF-16, ");
+        sNovo = new String(bytes, "windows-1252");
         System.out.println(sNovo);
 
         bytes = s.getBytes(StandardCharsets.US_ASCII);
-        System.out.println(bytes.length + ", US_ASCII");
-        sNovo = new String(bytes);
+        System.out.print(bytes.length + ", US_ASCII, ");
+        sNovo = new String(bytes, "windows-1252");
         System.out.println(sNovo);
     }
 }
